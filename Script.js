b55a7954a2b989ae1bd70977f59703b57b282aa9
Connor Watson <connorjosephwatson@hotmail.com>
2024-05-08 10:26:59 +0100

    moved files & added js for basket tab

--- Script.js
+++ Script.js
@@ -1,7 +1,17 @@
-console.log("Howdy doody");
+//open basket
 
-///////////////
-//Account Nav
+const basketBtn = document.getElementById("basket-button");
+const basketTab = document.getElementById("basket-tab");
 
-//////////////
-//Baket Tab
+basketBtn.textContent = "basket";
+
+basketBtn.addEventListener("click", function () {
+  basketTab.classList.remove("hidden-tab");
+});
+
+//close basket
+const closeBtn = document.getElementById("close");
+
+closeBtn.addEventListener("click", function () {
+  basketTab.classList.add("hidden-tab");
+});
